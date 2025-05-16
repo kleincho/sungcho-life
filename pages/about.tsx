b92b2ee71f832da810bb44459b5230f918fdb9d8
Sung Cho <chosungyun0912@gmail.comu>
2025-05-16 11:37:38 +0900

    Add initial template

---
+++ pages/about.tsx
@@ -0,0 +1,2 @@
+// About Page
+export default function About() { return <div>About Sungcho</div>; }
---
+++ pages/index.tsx
@@ -0,0 +1,2 @@
+// Home Page
+export default function Home() { return <div>Welcome to Sungcho's Site</div>; }

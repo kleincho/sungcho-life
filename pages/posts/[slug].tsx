1a41498236dfa036a6133a5c623edf91104394d1
Sung Cho <chosungyun0912@gmail.comu>
2025-05-16 19:13:42 +0900

    another bug fix

--- pages/posts/[slug].tsx
+++ pages/posts/[slug].tsx
@@ -1,13 +1,36 @@
-import { MDXRemote } from 'next-mdx-remote';
-import { getMdxContent } from '../../lib/mdx';
+import { GetStaticProps, GetStaticPaths, GetStaticPropsContext } from 'next';
+import { MDXRemote, MDXRemoteSerializeResult } from 'next-mdx-remote';
 import Layout from '../../components/Layout';
+import { getPostBySlug } from '../../lib/mdx';
 
-export default function PostPage({ source }) {
+type Props = {
+  source: MDXRemoteSerializeResult;
+};
+
+export default function PostPage({ source }: Props) {
   return (
     <Layout>
-      <div className="prose prose-lg font-body dark:prose-invert">
+      <div className="prose font-body dark:prose-invert">
         <MDXRemote {...source} />
       </div>
     </Layout>
   );
 }
+
+export const getStaticProps: GetStaticProps = async (
+  context: GetStaticPropsContext
+) => {
+  const slug = context.params?.slug as string;
+  const { source } = await getPostBySlug(slug);
+
+  return {
+    props: { source },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  return {
+    paths: [], // 추후 자동 처리 가능
+    fallback: false,
+  };
+};

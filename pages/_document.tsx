5b4884eeb05ac1681d027a31d5c8e6ee6519e176
Sung Cho <chosungyun0912@gmail.comu>
2025-05-20 22:43:01 +0900

    changed font to Space Grotesk

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,7 +6,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <link href="https://fonts.googleapis.com/css2?family=Epilogue:wght@300;400;600;700&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=Space+Grotesk:wght@300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
       <body>
         <Main />
